36ef8c45d27ef70cfac8b71cc840b64c7ae4f428
George Gil <georgegigarib@gmail.com>
2022-04-25 14:17:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {useState, useEffect} from 'react';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Quotebox from'./components/quote-box.component.jsx'
+//https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json
+
+const App = () => {
+
+  let [Allquotes, setQuotes] = useState([]);
+  let [newQuote, setQuote] = useState({});
+   var myNumber = 1;
+
+  useEffect(() => {
+    const myQuotes =  fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+    .then(response => response.json())
+    .then(data => {
+      setQuotes(data)}
+      );
+  }, []);
+
+async function getQuote() {
+    let myQuotes = await Allquotes.quotes;
+    let index = Math.floor(Math.random() * (102/*max*/ - 1/*min*/) + 1/*min*/);
+    setQuote(myQuotes[index]);
+}
+
+useEffect(() => {
+    getQuote();
+}, );
+
   return (
     <div className="App">
+    <h1>Random Quote Machine</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Quotebox quotes = {newQuote} getQuote = {getQuote}/>
       </header>
     </div>
   );
---
+++ src/components/quote-box.component.jsx
@@ -0,0 +1,24 @@
+import './quote-box.style.css';
+import { FaTwitter } from "react-icons/fa";
+
+const Quotebox = ({quotes, getQuote}) => {
+    
+    var {quote, author} = quotes;
+        return(
+            <div id="wrapper">
+            <div id="quote-box">
+            <div id="text">
+           "{quote}"
+            </div>
+            <div id="author">
+            -{author}
+            </div>
+            <a title="Tweet this quote!" href={`twitter.com/intent/tweet?hashtags=quotes&text=${quote}`} id="tweet-quote" target="_top"><FaTwitter /></a>
+            <button id="new-quote" onClick={getQuote}>New Quote</button>
+          </div>
+          </div>
+    )
+}
+
+export default Quotebox;
+
